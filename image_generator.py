13a0805b57b6c9559e4607e9ea0b383b4204aa78
thatwonguy <monsore1337@gmail.com>
2024-03-27 23:58:40 -0700

    testing new diffuser text-to-image generator model

--- image_generator.py
+++ image_generator.py
@@ -1,12 +1,17 @@
-from diffusers import StableDiffusionPipeline
+# from diffusers import StableDiffusionPipeline
+from diffusers import DiffusionPipeline
 import torch
 import streamlit as st
 
 @st.cache_resource
 def pipe_model():
-    model_id = "runwayml/stable-diffusion-v1-5"
-    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
-    # pipe = pipe.to("cuda")
+    model_id = "playgroundai/playground-v2-1024px-aesthetic"
+    pipe = DiffusionPipeline.from_pretrained(model_id,
+                                            torch_dtype=torch.float32,
+                                            use_safetensors=True,
+                                            add_watermarker=False,
+                                            variant="fp16"
+                                            )
     return pipe
 
 prompt = st.text_input("Write a description of the image you want to generate:")
