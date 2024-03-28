99379e8d488e3b25a0333a52e0a6f7f74e0eba28
thatwonguy <monsore1337@gmail.com>
2024-03-27 22:32:34 -0700

    updated script to get input and show output, with additional requirements

--- image_generator.py
+++ image_generator.py
@@ -8,4 +8,4 @@ pipe = pipe.to("cuda")
 prompt = "a photo of an astronaut riding a horse on mars"
 image = pipe(prompt).images[0]  
     
-image.save("astronaut_rides_horse.png")
+image.save("test.png")
